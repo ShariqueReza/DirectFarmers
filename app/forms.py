e00bae5282aa7e4783bb4e6342e549d86d3cc22b
Sharique <shariquereza786@gmail.com>
2025-03-25 12:32:22 +0530

    fixed some style in forms.py

--- app/forms.py
+++ app/forms.py
@@ -6,9 +6,9 @@ class Contact_UsForm(forms.ModelForm):
         model=contact_us
         fields={'name','email','subject','message'}
         widgets={
-            'name':forms.TextInput(attrs={'class':'form-control bg-light border-0 px-4','placeholder':'Enter your Name'}),
-            'email':forms.EmailInput(attrs={'class':'form-control bg-light border-0 px-4','placeholder':'Enter your Email'}),
-            'subject':forms.TextInput(attrs={'class':'form-control bg-light border-0 px-4','placeholder':'Enter your Subject'}),
-            'message':forms.TextInput(attrs={'class':'form-control bg-light border-0 px-4','placeholder':'Enter your Message'}),
+            'name':forms.TextInput(attrs={'class':'form-control bg-light border-0 px-4','placeholder':'Enter your Name','style': 'height: 55px;'}),
+            'email':forms.EmailInput(attrs={'class':'form-control bg-light border-0 px-4','placeholder':'Enter your Email','style': 'height: 55px;'}),
+            'subject':forms.TextInput(attrs={'class':'form-control bg-light border-0 px-4','placeholder':'Enter your Subject','style': 'height: 55px;'}),
+            'message':forms.TextInput(attrs={'class':'form-control bg-light border-0 px-4','placeholder':'Enter your Message','style': 'height: 55px;'}),
             
         }
