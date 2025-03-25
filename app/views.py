0012e5b5050b874158c0e3828e22354ab0b1c370
Sharique <shariquereza786@gmail.com>
2025-03-25 12:34:38 +0530

    adde logic for contact_usForm

--- app/views.py
+++ app/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from app.forms import Contact_UsForm
 # Create your views here.
 def index_page(request):
     context={}
     return render(request,'app/index.html',context)
 
 def contact_page(request):
-    context={}
+    form=Contact_UsForm()
+    if request.method == 'POST':
+        form = Contact_UsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+        else:
+            form = Contact_UsForm()
+    context={'form':form}
     return render(request,'app/contact.html',context)
