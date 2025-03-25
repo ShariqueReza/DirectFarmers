170957769b53690482c80d851159b41bd2657b57
Sharique <shariquereza786@gmail.com>
2025-03-25 11:58:08 +0530

    made contact_us model for contact.html page

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class contact_us(models.Model):
+    name=models.CharField(max_length=100,null=True,blank=True)
+    email=models.EmailField(max_length=100,null=True)
+    subject=models.CharField(max_length=100,null=True,blank=True)
+    message=models.CharField(max_length=1000,null=True,blank=True)
+    
+    
