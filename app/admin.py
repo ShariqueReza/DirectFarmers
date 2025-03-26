9cb8cd9fb8de3366308524b68cc8399cdb3e4ee6
Sharique <shariquereza786@gmail.com>
2025-03-26 12:55:25 +0530

    registered Area,Product model

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from app.models import contact_us
-
+from app.models import contact_us,Area,Product
 
 # Register your models here.
 admin.site.register(contact_us)
+admin.site.register(Area)
+admin.site.register(Product)
